72c87cec41c63c228d705bde1d5b780ed5a4e12c
Your Name <you@example.com>
2026-05-20 00:01:11 +0530

    jenkinsfileupdated

--- client/src/api.js
+++ client/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || '',
+  baseURL: 'https://inkwell-server-7l9m.onrender.com',
 });
 
 export default api;
